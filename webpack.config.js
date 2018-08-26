77b7790de42ac50599c102e3dd34215bffd096cc
nu11p01n73R <kjjohny1991@gmail.com>
2018-08-27 01:03:19 +0530

    webpack dev server base changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 var path = require('path')
 var VueLoaderPlugin = require('vue-loader/lib/plugin')
 
+const isProd = process.env.NODE_ENV == 'production'
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
+        path: path.resolve(
+            __dirname, 
+            isProd ? './dist' : './src'),
         filename: 'main.js'
     },
     mode: process.env.NODE_ENV,
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: true,
+    },
     resolve: {
         alias: {
             'vue$': 'vue/dist/vue.esm.js'
